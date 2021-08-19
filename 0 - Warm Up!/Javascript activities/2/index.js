2818c76999c0fdd0bc2ba364d39230b3b311e2ef
rodriguesms <miguelrodrigues@eng.ci.ufpb.br>
2021-08-19 10:27:20 -0300

    Even numbers task: done, pagination: done

--- 0 - Warm Up!/Javascript activities/2/index.js
+++ 0 - Warm Up!/Javascript activities/2/index.js
@@ -1,18 +1,29 @@
 // Write a program that calculates the quantity of even numbers in an randomly generated array containing ten unsigned integers
-const evenQuantArray = (max_value=1000) => {
-    var randomArray = [];
-    var arraySize = 10;
-    var pairQuant = 0;
+const evenQuantArray = (max_value) => {
 
-    for(i = 0; i < arraySize; i++){
-        randomArray.push(Math.floor(Math.random() * max_value)); // Generates random integer number between 0 and max_value
-    }
+    Number.isNaN(max_value) ? max_value=1000 : max_value=max_value; // if input parameter is empty (""), parseInt sends NaN
 
-    randomArray.forEach(number => {
-        if(number%2 == 0){ // if is it pair
-            pairQuant++;
+    if(max_value > Number.MAX_SAFE_INTEGER)    
+        alert("ERROR: Selected number is too large!");
+
+    else if(max_value < 0)
+        alert("ERROR: Selected number < 0!");
+
+     else {
+        var randomArray = [];
+        var arraySize = 10;
+        var pairQuant = 0;
+    
+        for(i = 0; i < arraySize; i++){
+            randomArray.push(Math.floor(Math.random() * max_value));
         }
-    });
-
-    return pairQuant;
+    
+        randomArray.forEach(number => {
+            if(number%2 == 0){
+                pairQuant++;
+            }
+        });
+        
+        alert(`Max value: ${max_value}\n\nRandom array: [${randomArray}]\n\nEven numbers quantity: ${pairQuant}`);
+    }    
 }
