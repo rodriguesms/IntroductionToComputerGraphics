2b6c0516815b6a55358b2d2fdb05399ed1d05123
rodriguesms <miguelrodrigues@eng.ci.ufpb.br>
2021-09-09 22:28:18 -0300

    changing rgb background of canvas

--- 1 - Line Rasterization/1 - Algorithm implementation/algorithms.js
+++ 1 - Line Rasterization/1 - Algorithm implementation/algorithms.js
@@ -1,8 +1,9 @@
 class Canvas {
   constructor(canvas_id) {
+    console.log(canvas_id)
     this.canvas = document.getElementById(canvas_id);
     this.context = this.canvas.getContext("2d");
-    this.clear_color = 'rgba(0,0,0,255)';
+    this.clear_color = 'rgba(0,0,0,0)';
   }
 
   clear() {
@@ -16,6 +17,7 @@ class Canvas {
   }
 }
 
+console.log("carregou")
 
 let color_buffer = new Canvas("midpoint-canvas");
 color_buffer.clear();
@@ -24,28 +26,10 @@ function MidPointLineAlgorithm(x0, y0, x1, y1, color_0, color_1) {
 	// Escreva seu código aqui!
   
   // Exemplo de código para acender pixels no canvas:
-  let dx = Math.abs(x1 - x0);
-  let dy = -Math.abs(y1 - y0);
-  let sx = (x1 > x0) ? 1 : -1;
-  let sy = (y1 - y0) ? 1 : -1;
-  let error = dx + dy;
-  while(1)
-  {
-    color_buffer.putPixel(x0, y0);
-    if(x0 == x1 && y0 == y1) 
-      break;
-    error2 = 2 * error;
-    if(error2 >= dy)
-    {
-      error += dy;
-      x0 += sx;
-    }
-    if(e2 <= dx)
-    {
-      error += dx;
-      y0 += sy;
-    }
-  }
+  for (let y = y0; y < y1; ++y)
+    for (let x = x0; x < x1; ++x)
+      color_buffer.putPixel(x, y, color_0); 
+
 }
 
 function DrawTriangle(x0, y0, x1, y1, x2, y2, color_0, color_1, color_2) {
