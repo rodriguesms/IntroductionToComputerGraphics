5cc9e229dae8e6b5ba13bdbf4269fa94ce92126d
Isaac-CI <isaacmarinho@eng.ci.ufpb.br>
2021-09-09 21:52:29 -0300

    Generic Bresenham's line algorithm implementation

--- 1 - Line Rasterization/1 - Algorithm implementation/algorithms.js
+++ 1 - Line Rasterization/1 - Algorithm implementation/algorithms.js
@@ -5,9 +5,28 @@ function MidPointLineAlgorithm(x0, y0, x1, y1, color_0, color_1) {
 	// Escreva seu código aqui!
   
   // Exemplo de código para acender pixels no canvas:
-  for (let y = y0; y < y1; ++y)
-    for (let x = x0; x < x1; ++x)
-      color_buffer.putPixel(x, y, color_0); 
+  let dx = Math.abs(x1 - x0);
+  let dy = -Math.abs(y1 - y0);
+  let sx = (x1 > x0) ? 1 : -1;
+  let sy = (y1 - y0) ? 1 : -1;
+  let error = dx + dy;
+  while(1)
+  {
+    color_buffer.putPixel(x0, y0);
+    if(x0 == x1 && y0 == y1) 
+      break;
+    error2 = 2 * error;
+    if(error2 >= dy)
+    {
+      error += dy;
+      x0 += sx;
+    }
+    if(e2 <= dx)
+    {
+      error += dx;
+      y0 += sy;
+    }
+  }
 }
 
 function DrawTriangle(x0, y0, x1, y1, x2, y2, color_0, color_1, color_2) {
